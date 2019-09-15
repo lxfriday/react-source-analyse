509c081716daf65fec7b0c73fb532ce82bed10e3
lxfriday <liu3248184446@outlook.com>
2019-09-16 00:59:49 +0800

    feat: prepare ci

--- config/paths.js
+++ config/paths.js
@@ -34,7 +34,7 @@ const getPublicUrl = appPackageJson =>
 function getServedPath(appPackageJson) {
   const publicUrl = getPublicUrl(appPackageJson);
   const servedUrl =
-    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
+    envPublicUrl || (publicUrl ? '/' + url.parse(publicUrl).pathname.split('/')[2] : '/');
   return ensureSlash(servedUrl, true);
 }
 
