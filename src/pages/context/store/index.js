e5c7879ea189fec9b328e2b88d7d097d359b5fda
lxfriday <liu3248184446@outlook.com>
2019-09-16 19:10:06 +0800

    fix: 添加自定义 middleware

--- src/pages/context/store/index.js
+++ src/pages/context/store/index.js
@@ -1,8 +1,22 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { createLogger } from 'redux-logger'
 import rootReducer from '../reducers'
-const middlewares = [createLogger()]
+
+function loggerMiddleware(storeContext) {
+  return function(nextDispatch) {
+    // 上面的 store.dispatch
+    return function(action) {
+      console.log('before', storeContext.getState())
+      nextDispatch(action)
+      console.log('after', storeContext.getState())
+    }
+  }
+}
+
+const middlewares = [loggerMiddleware]
+
 const enhancer = compose(applyMiddleware(...middlewares))
+
 export default function configStore() {
   const store = createStore(rootReducer, enhancer)
   return store
